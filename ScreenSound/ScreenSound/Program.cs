4f7555dcb965857db037689dc29bf04b03242bac
DanilloIsaque <danilloisaq@gmail.com>
2025-12-27 17:40:19 -0300

    classes e metodos

---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,24 @@
+﻿Banda slipknot = new Banda("Slipknot");
+
+Album albumDoSlipknot = new Album("We are not your kind");
+
+Musica musica1 = new Musica(slipknot, "Unsainted")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(slipknot, "Solway Firth")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+albumDoSlipknot.AdicionarMusica(musica1);
+albumDoSlipknot.AdicionarMusica(musica2);
+slipknot.AdicionarAlbum(albumDoSlipknot);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumDoSlipknot.ExibirMusicasDoAlbum();
+slipknot.ExibirDiscografia();
