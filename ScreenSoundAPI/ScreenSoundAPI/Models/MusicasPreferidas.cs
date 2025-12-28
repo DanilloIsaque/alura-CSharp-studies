7464b8b1fa633e748aa8042466c2c07416e81345
DanilloIsaque <danilloisaq@gmail.com>
2025-12-28 19:03:13 -0300

    feature: consumindo api, manipulando JSON, criando arquivo Json e Linq

---
+++ ScreenSoundAPI/ScreenSoundAPI/Models/MusicasPreferidas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace ScreenSoundAPI.Models
+{
+    internal class MusicasPreferidas
+    {
+        public string Nome { get; set; }
+        public List<Musica> ListaDeMusicas { get; set; }
+
+        public MusicasPreferidas(string nome)
+        {
+            Nome = nome;
+            ListaDeMusicas = new List<Musica>();
+        }
+
+        public void AdicionarMusica(Musica musica)
+        {
+            ListaDeMusicas.Add(musica);
+        }   
+
+        public void ExibirMusicas()
+        {
+            Console.WriteLine($"Músicas preferidas de {Nome}:");
+            foreach (var musica in ListaDeMusicas)
+            {
+                Console.WriteLine($"- {musica.Nome} por {musica.Artista}");
+            }
+        }
+
+        public void GerarArquivoJson()
+        {
+            string json = JsonSerializer.Serialize(new
+            {
+                Nome = this.Nome,
+                Musicas = this.ListaDeMusicas
+            });
+            string nomeArquivo = $"{Nome}_musicas_preferidas.json";
+
+            File.WriteAllText(nomeArquivo, json);
+            Console.WriteLine($"Arquivo JSON gerado: {nomeArquivo} em {Path.GetFullPath(nomeArquivo)}");
+
+        }
+
+    }
+}
---
+++ ScreenSoundAPI/ScreenSoundAPI/Program.cs
@@ -0,0 +1,59 @@
+﻿using ScreenSoundAPI.Filtros;
+using ScreenSoundAPI.Models;
+using System.Linq;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://" +
+            "guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        //Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+        musicas[0].ExibirDetalhesMusica();
+
+        LinqFilter.FiltrarGeneros(musicas);
+        LinqFilter.FiltrarMusicasPorArtistas(musicas, "Travis Scott");
+        LinqOrder.exibirArtistasOrdenados(musicas);
+        LinqFilter.FiltrarArtistasPorGenero(musicas, "rap");
+
+        var musicasPreferidas = new MusicasPreferidas("Danillo");
+        musicasPreferidas.AdicionarMusica(musicas[10]);
+        musicasPreferidas.AdicionarMusica(musicas[1299]);
+        musicasPreferidas.AdicionarMusica(musicas[2]);
+        musicasPreferidas.AdicionarMusica(musicas[4]);
+        musicasPreferidas.AdicionarMusica(musicas[110]);
+        musicasPreferidas.ExibirMusicas();
+        musicasPreferidas.GerarArquivoJson();
+
+        //// Ordenar por nome ascendente
+        //var ordenadasPorNome = musicas.OrderBy(m => m.Nome).ToList();
+        //Console.WriteLine("\nOrdenadas por nome (asc):");
+        //foreach (var m in ordenadasPorNome.Take(10))
+        //{
+        //    Console.WriteLine($"{m.Nome} - {m.Artista}");
+        //}
+
+        //// Filtrar por gênero contendo "Rock"
+        //var rock = musicas.Where(m => (m.Genero ?? "").ToLower().Contains("rock")).ToList();
+        //Console.WriteLine($"\nMúsicas com gênero 'rock' ({rock.Count}):");
+        //foreach (var m in rock.Take(10))
+        //{
+        //    Console.WriteLine($"{m.Nome} - {m.Artista} - {m.Genero}");
+        //}
+
+        //// Filtrar por artista (contains)
+        //var artistaBusca = "Queen";
+        //var porArtista = musicas.Where(m => (m.Artista ?? "").IndexOf(artistaBusca, StringComparison.OrdinalIgnoreCase) >= 0);
+        //Console.WriteLine($"\nMúsicas que contém '{artistaBusca}' no artista:");
+        //foreach (var m in porArtista)
+        //{
+        //    Console.WriteLine($"{m.Nome} - {m.Artista}");
+        //}
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
